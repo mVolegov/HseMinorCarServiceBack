0bf968e02b7a7b2a3ffbf223d125ae2d5567d537
mVolegov <volegovmikhail12@gmail.com>
2023-01-28 21:01:49 +0500

    Add new repair request status - ACCEPTED

--- src/main/kotlin/com/mvoleg/hseminorcarserviceback/entity/CarRepairRequestStatus.kt
+++ src/main/kotlin/com/mvoleg/hseminorcarserviceback/entity/CarRepairRequestStatus.kt
@@ -2,6 +2,7 @@ package com.mvoleg.hseminorcarserviceback.entity
 
 enum class CarRepairRequestStatus(val statusName: String) {
     NONE("Нет статуса"),
+    ACCEPTED("Принято"),
     IN_PROGRESS("В работе"),
     DONE("Сделано")
 }
