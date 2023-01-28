9b25aad216ab1261effcc703e4e45addebe313fe
mVolegov <volegovmikhail12@gmail.com>
2023-01-28 19:55:45 +0500

    Add new mapping method to map repair request dto to entity class

--- src/main/kotlin/com/mvoleg/hseminorcarserviceback/mapper/Mapper.kt
+++ src/main/kotlin/com/mvoleg/hseminorcarserviceback/mapper/Mapper.kt
@@ -5,11 +5,22 @@ import com.mvoleg.hseminorcarserviceback.entity.CarEntity
 import com.mvoleg.hseminorcarserviceback.entity.CarRepairRequestArchiveEntity
 import com.mvoleg.hseminorcarserviceback.entity.CarRepairRequestEntity
 import com.mvoleg.hseminorcarserviceback.entity.ClientEntity
-import org.hibernate.annotations.Comment
 
 class Mapper {
 
     companion object {
+
+        fun mapCarRepairRequestDTOtoEntity(dto: CarRepairRequestDTO): CarRepairRequestEntity {
+            return CarRepairRequestEntity(
+                id = dto.id ?: 0L,
+                client = extractClientEntityFromCarRepairRequestDTO(dto),
+                car = extractCarEntityFromCarRepairRequestDTO(dto),
+                appealReason = dto.appealReason,
+                declaredWorks = dto.declaredWorks,
+                totalPriceOfWorks = dto.totalPriceOfWorks,
+                status = dto.status
+            )
+        }
 
         fun mapCarRepairRequestEntityToDTO(entity: CarRepairRequestEntity): CarRepairRequestDTO {
             return CarRepairRequestDTO(
